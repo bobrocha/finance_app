841375a352c65698d180fc09d1f43c79c64bdd06
Robert Rocha <oxconcise@hotmail.com>
2019-12-09 20:10:48 -0500

    Creating first branch, contains basic stuff, categories controller, model and vue components

---
+++ resources/js/components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+	<div v-show="display_error">
+		<div class="alert alert-danger" role="alert">{{ error_message }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name : 'error',
+	props : {
+		display_error : {
+			type    : Boolean,
+			default : false,
+		},
+		error_message : {
+			type    : String,
+			default : '',
+		},
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/categories/CategoriesDataTableFoot.vue
@@ -0,0 +1,49 @@
+<template>
+	<tfoot>
+		<tr>
+			<td colspan="3" class="pagination-buttons">
+				<nav aria-label="Cateogry resutls">
+					<ul class="pagination justify-content-center">
+						<li
+							v-show="many_links" v-for="(link, index) in links"
+							:key="index"
+							class="page-item"
+						><a class="page-link" href="#">{{ index }}</a></li>
+						<!-- <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+						<li class="page-item"><a class="page-link" href="#">1</a></li>
+						<li class="page-item"><a class="page-link" href="#">2</a></li>
+						<li class="page-item"><a class="page-link" href="#">3</a></li>
+						<li class="page-item"><a class="page-link" href="#">Next</a></li> -->
+					</ul>
+				</nav>
+			</td>
+		</tr>
+	</tfoot>
+</template>
+
+<script>
+export default {
+	name  : 'categories-data-table-foot',
+	props : {
+		page_quantity : Number,
+	},
+	data() {
+		return {
+		}
+	},
+	computed : {
+		links() {
+			return [...Array(this.page_quantity).keys()];
+		},
+		many_links() {
+			return this.page_quantity > 0;
+		}
+	}
+}
+</script>
+
+<style>
+.table-container .pagination {
+	padding-top: 1rem;
+}
+</style>
